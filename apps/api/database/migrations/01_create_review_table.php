81a3df53b7d179535d8ad7512f165c788121203b
Lucas Chagas <43484254+Lucas-chagas@users.noreply.github.com>
2026-09-10 12:55:49 -0300

    Add files via upload

---
+++ apps/api/database/migrations/01_create_review_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('review', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id')->unsigned();
+            $table->integer('customer_id')->unsigned();
+            $table->integer('rating')->unsigned();
+            $table->text('comment');
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cache');
+        Schema::dropIfExists('cache_locks');
+    }
+};
+
+
+
+
+/*
+        Schema::create('cache_locks', function (Blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->bigInteger('expiration')->index();
+        });
+
+*/
