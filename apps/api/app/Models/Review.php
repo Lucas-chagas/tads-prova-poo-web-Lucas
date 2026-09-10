c28450ecc1ea3fe149bec520efc0b94186d019a7
Lucas Chagas <43484254+Lucas-chagas@users.noreply.github.com>
2026-09-10 12:58:30 -0300

    Add files via upload

---
+++ apps/api/app/Models/Review.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+#[Fillable(['product_id', 'customer_id', 'rating'])]
+
+class Review extends Authenticatable
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'product_id' => 'integer',
+            'customer_id' => 'integer',
+            'rating' => 'integer',
+        ];
+    }
+}
